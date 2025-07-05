fd0830442f02b842c678be1fca0ba47f27f0e50e
EnCryptoknight <141088689+kherleefer@users.noreply.github.com>
2025-07-05 18:45:02 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
+//import Link from "next/link";
 import { Github, Send, Twitter, Mail } from "lucide-react";
 import { motion } from "framer-motion";
 
