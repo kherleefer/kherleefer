e4b22e5602779079bd76e60ac2cf3e9c7a311de8
EnCryptoknight <141088689+kherleefer@users.noreply.github.com>
2025-07-05 12:14:47 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,9 @@ export default function Home() {
           </motion.div>
           <h1 className="text-4xl font-bold mt-4">
             Kherleefer
-            <p><small>(Encryptoknight)</small></p>
+            <p>
+              <small>(Encryptoknight)</small>
+            </p>
           </h1>
           <p className="text-lg text-blue-200">Software Engineer</p>
           <div className="flex justify-center gap-4 mt-4">
@@ -73,38 +75,47 @@ export default function Home() {
           className="mb-12"
         >
           <h2 className="text-2xl font-semibold mb-6">Projects</h2>
-          <motion.div 
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="grid grid-cols-1 md:grid-cols-3 gap-4"
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="grid grid-cols-1 md:grid-cols-3 gap-4"
           >
             {[
               {
                 title: "Gloxx Chain",
                 description:
-                  "Developing a web application with Ionic Vue and Supabase PostgreSQL",
+                  "Developing a web application with Ionic Vue and Supabase PostgreSQL, Thanks to capacitor",
+                link: "https://t.me/gloxx_chain/20",
+              },
+              {
+                title: "Gloxx Chain API",
+                description:
+                  "Create an API for Gloxx chain Crypto mining phase using ExpressJS and PostgreSQL on Supabase. I am currently bulding the Blockcahan anssmart contract using Rust.",
                 link: "#",
               },
               {
-                title: "API UI",
+                title: "Email Filter App",
                 description:
-                  "Creating a user interface for an Express API using Vue.js",
-                link: "#",
-              },
-              {
-                title: "GetFintigration",
-                description:
-                  "Adding Telegram Mini Apps Analytics to a React application",
-                link: "#",
+                  " develop Email filter app for Detecting Spam and Phishing Emails using HTML5, PHP, and Python for ML implementation. the project is currently in progress or rather on hold.",
+                link: "https://thinkdoit.infy.uk",
               },
             ].map((project, index) => (
               <motion.div
                 key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
                 whileHover={{ scale: 1.03 }}
+                transition={{ duration: 0.6 }}
                 className="bg-blue-700 p-4 rounded-2xl"
               >
-                <h3 className="text-lg font-bold mb-2">{project.title}</h3>
+                <motion.h3
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  className="text-lg font-bold mb-2"
+                >
+                  {project.title}
+                </motion.h3>
                 <p className="text-blue-100 text-sm mb-4">
                   {project.description}
                 </p>
@@ -126,13 +137,13 @@ export default function Home() {
           className="mb-12"
         >
           <h2 className="text-2xl font-semibold mb-4">Skills</h2>
-          <motion.div 
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="flex flex-wrap gap-3"
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="flex flex-wrap gap-3"
           >
-            {["Ionic", "Vue.js", "TypeScript", "React"].map((skill) => (
+            {["Ionic", "Vue.js", "TypeScript", "React", "NextJs", "ExpressJs", "RUST", "TACT", "etc"].map((skill) => (
               <span
                 key={skill}
                 className="bg-blue-600 px-3 py-1 rounded-full text-sm"
@@ -148,7 +159,7 @@ export default function Home() {
           transition={{ duration: 0.5 }}
           viewport={{ once: true }}
         >
-          <h2 className="text-2xl font-semibold mb-4">Contact</h2>
+          <h2 className="text-2xl font-semibold mb-4">Leave A Remark</h2>
           <form className="bg-blue-700 p-6 rounded-2xl">
             <input
               type="text"
@@ -173,6 +184,62 @@ export default function Home() {
             </button>
           </form>
         </motion.section>
+        <motion.section
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          viewport={{ once: true }}
+        >
+          <h2 className="text-2xl font-semibold mb-4">Others Remarks</h2>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="grid grid-cols-1 md:grid-cols-3 gap-4"
+          >
+            {[
+              {
+                title: "Zik Stane",
+                description:
+                  "Developing a web application with Ionic Vue and Supabase PostgreSQL, Thanks to capacitor",
+                link: "https://t.me/gloxx_chain/20",
+              },
+              {
+                title: "Mehmad",
+                description:
+                  "Create an API for Gloxx chain Crypto mining phase using ExpressJS and PostgreSQL on Supabase. I am currently bulding the Blockcahan anssmart contract using Rust.",
+                link: "#",
+              },
+              {
+                title: "Rose Adam",
+                description:
+                  " develop Email filter app for Detecting Spam and Phishing Emails using HTML5, PHP, and Python for ML implementation. the project is currently in progress or rather on hold.",
+                link: "https://thinkdoit.infy.uk",
+              },
+            ].map((project, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                whileHover={{ scale: 1.03 }}
+                transition={{ duration: 0.6 }}
+                className="bg-blue-700 p-4 rounded-2xl"
+              >
+                <motion.h3
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  className="text-lg font-bold mb-2"
+                >
+                  {project.title}
+                </motion.h3>
+                <p className="text-blue-100 text-sm mb-4">
+                  {project.description}
+                </p>
+                
+              </motion.div>
+            ))}
+          </motion.div>
+         </motion.section> 
       </div>
     </motion.div>
   );
