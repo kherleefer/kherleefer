0c89cc8ac8d913f4ba1a592902cf0d9ceca5ca49
EnCryptoknight <141088689+kherleefer@users.noreply.github.com>
2025-09-09 19:14:57 +0100

    Update DesktopOnly.tsx

--- components/DesktopOnly.tsx
+++ components/DesktopOnly.tsx
@@ -1,18 +1,19 @@
 "use client";
 
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 
 export default function DesktopOnly({ children }: { children: React.ReactNode }) {
+  {/*
   const [isDesktop, setIsDesktop] = useState(true);
   const [typedText, setTypedText] = useState("");
 
   const terminalText = '$ echo "Desktop required ⚠️"';
 
   useEffect(() => {
-   // const checkSize = () => setIsDesktop(window.innerWidth >= 1024);
-  //  checkSize();
-   // window.addEventListener("resize", checkSize);
-  //  return () => window.removeEventListener("resize", checkSize);
+    const checkSize = () => setIsDesktop(window.innerWidth >= 1024);
+    checkSize();
+    window.addEventListener("resize", checkSize);
+    return () => window.removeEventListener("resize", checkSize);
   }, []);
 
   // Typewriter effect
@@ -27,12 +28,13 @@ export default function DesktopOnly({ children }: { children: React.ReactNode })
       return () => clearInterval(interval);
     }
   }, [isDesktop]);
-
+*/
+    {/*
   if (!isDesktop) {
     return (
       <div className="w-screen h-screen flex items-center justify-center bg-black font-mono text-green-400 p-6 overflow-hidden">
         <div className="bg-black border border-green-400 rounded-lg p-6 shadow-lg max-w-md w-full">
-          {/* Terminal prompt */}
+          
           <pre className="whitespace-pre-wrap break-words text-green-400">
             {typedText}
             <span className="inline-block animate-blink">█</span>
@@ -46,6 +48,6 @@ export default function DesktopOnly({ children }: { children: React.ReactNode })
       </div>
     );
   }
-
+*/}
   return <>{children}</>;
 }
