819a411f670e001c5e65cdb2a18930f828a011e7
EnCryptoknight <141088689+kherleefer@users.noreply.github.com>
2025-09-09 11:29:02 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 //import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import DesktopOnly from "@components/DesktopOnly";
+
 /*
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +28,18 @@ export default function RootLayout({
     <html lang="en">
       <body
         className="antialiased">
-        {children}
+        <DesktopOnly>{children}</DesktopOnly>
       </body>
     </html>
   );
 }
+
+
+
+
+
+
+
+
+
+
