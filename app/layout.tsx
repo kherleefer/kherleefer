08a12c72e15ac3991b315b50e38c5198108bd1d6
EnCryptoknight <141088689+kherleefer@users.noreply.github.com>
2025-09-10 19:51:00 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import DesktopOnly from "@components/DesktopOnly";
+//import DesktopOnly from "@components/DesktopOnly";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 
@@ -59,7 +59,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased text-zinc-100 font-mono relative min-h-screen">
-         <DesktopOnly>
+        {  /* <DesktopOnly>*/}
           {/* Global background image */}
           <div
             className="fixed inset-0 -z-10"
@@ -79,7 +79,7 @@ export default function RootLayout({
           <main className="pt-10 relative z-10">{children}</main>
          
           <Footer />
-        </DesktopOnly>
+        {/* </DesktopOnly>*/}
       </body>
     </html>
   );
